28cfa54868d20338304954b5ec999b34159618ca
CodeCrafterRohit <rohitadhav045@gmail.com>
2025-03-27 16:24:55 +0530

    Project Related Data Added

--- JAVASCRIPT/DAY-13/dynamickey.js
+++ JAVASCRIPT/DAY-13/dynamickey.js
@@ -1,7 +1,14 @@
-let newName = "John";
-let newKeyName = "dynamicKey";
 
-let employee = {
-  [newKeyName]: newName,
-};
-console.log(employee);
+
+let human = {
+  name: "John",
+  age: 20,
+  address: {
+      city:"Chennai"
+  }
+}
+
+console.log(human.name)
+// console.log(human.city.name)
+console.log(human.address.city)
+console.log(human?.address?.pincode?.number)
